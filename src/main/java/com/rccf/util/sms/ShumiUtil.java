c3039e38d0e4f00f2a6d273f35d328c7010a7a60
maojinhui_work@163.com <mjh521mjh>
2017-07-25 18:42:23 +0800

    发送短信以及保存短信记录

--- src/main/java/com/rccf/util/sms/ShumiUtil.java
+++ src/main/java/com/rccf/util/sms/ShumiUtil.java
@@ -10,7 +10,6 @@ import com.rccf.util.SpringContextUtil;
 import com.rccf.util.encrypt.Base64;
 import com.rccf.util.encrypt.Md5Encrypt;
 import com.rccf.util.network.HttpUtil;
-import org.apache.http.cookie.SM;
 import org.apache.log4j.Logger;
 
 import java.text.DateFormat;
@@ -27,57 +26,59 @@ public class ShumiUtil {
 
     /**
      * 发送普通验证码短信
+     *
      * @param mobile
      * @return
      */
-    public static String sendSimple(String mobile){
+    public static String sendSimple(String mobile) {
         SmsService smsService = (SmsService) SpringContextUtil.getBean("smsServiceImpl");
         SpyMemcachedManager spyMemcachedManager = (SpyMemcachedManager) SpringContextUtil.getBean("memcachedManager");
-        DateFormat format=new SimpleDateFormat("yyyymmddhhmmss");
+        DateFormat format = new SimpleDateFormat("yyyymmddhhmmss");
         String timespan = format.format(new Date());
-        String pwd = Md5Encrypt.getMD5(AccountConstants.SHUMI_PWD+timespan).toUpperCase();
+        String pwd = Md5Encrypt.getMD5(AccountConstants.SHUMI_PWD + timespan).toUpperCase();
         Random random = new Random();
         int res = random.nextInt(9000) + 1000;
         String code = String.valueOf(res);
-        String _content = "【融成官网】"+code+"融成金服验证码，有效时间10分钟，请尽快验证";
+        String _content = "【融成官网】" + code + "融成金服验证码，有效时间10分钟，请尽快验证";
         String content = Base64.getBase64(_content);
-        Map<String,String> param=new HashMap<String, String>();
+        Map<String, String> param = new HashMap<String, String>();
         param.put("userid", AccountConstants.SHUMI_ID);
-        param.put("timespan",timespan);
-        param.put("pwd",pwd);
-        param.put("mobile",mobile);
-        param.put("msgfmt","UTF8");
-        param.put("content",content);
-        String result = HttpUtil.post(UrlConstants.URL_SHUMI_SEND_SIMPLE,param);
-        if (!result.contains("-")){
-            spyMemcachedManager.set(mobile,code,10*60);
-            Sms sms = getSms(mobile,code,result,1);
+        param.put("timespan", timespan);
+        param.put("pwd", pwd);
+        param.put("mobile", mobile);
+        param.put("msgfmt", "UTF8");
+        param.put("content", content);
+        String result = HttpUtil.post(UrlConstants.URL_SHUMI_SEND_SIMPLE, param);
+        if (!result.contains("-")) {
+            spyMemcachedManager.set(mobile, code, 10 * 60);
+            Sms sms = getSms(mobile, code, result, 1);
             smsService.save(sms);
-        }else{
-            Sms sms = getSms(mobile,code,result,Integer.valueOf(result));
+        } else {
+            Sms sms = getSms(mobile, code, result, Integer.valueOf(result));
             smsService.save(sms);
         }
-     return result;
+        return result;
     }
 
     /**
      * 查询余额
+     *
      * @return
      */
-    public static String getBalance(){
-        Map<String,String> param=new HashMap<String, String>();
-        DateFormat format=new SimpleDateFormat("yyyymmddhhmmss");
+    public static String getBalance() {
+        Map<String, String> param = new HashMap<String, String>();
+        DateFormat format = new SimpleDateFormat("yyyymmddhhmmss");
         String timespan = format.format(new Date());
-        String pwd = Md5Encrypt.getMD5(AccountConstants.SHUMI_PWD+timespan).toUpperCase();
+        String pwd = Md5Encrypt.getMD5(AccountConstants.SHUMI_PWD + timespan).toUpperCase();
         param.put("userid", AccountConstants.SHUMI_ID);
-        param.put("timespan",timespan);
-        param.put("pwd",pwd);
-        String result = HttpUtil.post(UrlConstants.URL_SHUMI_SEND_BALANCE,param);
+        param.put("timespan", timespan);
+        param.put("pwd", pwd);
+        String result = HttpUtil.post(UrlConstants.URL_SHUMI_SEND_BALANCE, param);
         return result;
     }
 
 
-    private static Sms getSms(String mobile,String code,String msgid,int resultCode){
+    private static Sms getSms(String mobile, String code, String msgid, int resultCode) {
         Sms sms = new Sms();
         sms.setCode(code);
         sms.setPhone(mobile);
