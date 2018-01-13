661f84ff9f78074d7cd72eaec9d8f7dc83e4d896
maojinhui_work@163.com <mjh521mjh>
2018-01-13 18:49:47 +0800

    受理单情况

--- src/main/java/com/rccf/controller/gzh/DataController.java
+++ src/main/java/com/rccf/controller/gzh/DataController.java
@@ -973,9 +973,9 @@ public class DataController {
 
         String sql_dupty = "\n" +
                 "SELECT e.id,e.`name` ,e.`department` , e.`code` , e.role ,\n" +
-                "(SELECT  COUNT(*) from `accepted`  a WHERE  a.`accept_time` >= '"+month_end+"' and a.`accept_time` < '"+month_end+"'  and  a.`director` =e.code ) as monthaccept,\n" +
-                "(SELECT COUNT(*) FROM accepted a WHERE a.`end_date` >= '"+month_start+"' and a.`end_date` < '"+month_end+"'  and  a.`director` =e.code  and `state` =2) as monthend ,\n" +
-                "(SELECT COUNT(*) FROM accepted a WHERE a.`create_time`  >= '"+month_start+"' and a.`create_time`< '"+month_end+"'  and  a.`director` =e.code AND (`state` =3 or `state` =4) ) as monthrefuse ,\n" +
+                "(SELECT  COUNT(*) from `accepted`  a WHERE  a.`accept_time` >= '"+month_end+"' and a.`accept_time` < '"+month_end+"'  and  a.`deputy_director` =e.code ) as monthaccept,\n" +
+                "(SELECT COUNT(*) FROM accepted a WHERE a.`end_date` >= '"+month_start+"' and a.`end_date` < '"+month_end+"'  and  a.`deputy_director` =e.code  and `state` =2) as monthend ,\n" +
+                "(SELECT COUNT(*) FROM accepted a WHERE a.`create_time`  >= '"+month_start+"' and a.`create_time`< '"+month_end+"'  and  a.`deputy_director` =e.code AND (`state` =3 or `state` =4) ) as monthrefuse ,\n" +
                 " (SELECT COUNT(*) FROM accepted a WHERE a.state = 1 and a.`director` = e.`code` AND a.business_type=0 ) as nowaccept_xindai,\n" +
                 " (SELECT COUNT(*) FROM accepted a WHERE a.state = 1 and a.`director` = e.`code` AND a.business_type=1 ) as nowaccept_diya,\n" +
                 " (SELECT COUNT(*) FROM accepted a WHERE a.state = 1 and a.`director` = e.`code` AND a.business_type=2 ) as nowaccept_zhiya,\n" +
@@ -984,9 +984,9 @@ public class DataController {
 
         if (employee.getDepartment().equals("金融渠道部")) {
             sql_dupty = "SELECT e.id,e.`name` ,e.`department` , e.`code` , e.role,\n" +
-                    "(SELECT  COUNT(*) from `accepted`  a WHERE  a.`accept_time` >= '"+month_end+"' and a.`accept_time` < '"+month_end+"'  and  a.`director` =e.code ) as monthaccept,\n" +
-                    "(SELECT COUNT(*) FROM accepted a WHERE a.`end_date` >= '"+month_start+"' and a.`end_date` < '"+month_end+"'  and  a.`director` =e.code  and `state` =2) as monthend,\n" +
-                    "(SELECT COUNT(*) FROM accepted a WHERE a.`create_time`  >= '"+month_start+"' and a.`create_time`< '"+month_end+"'  and  a.`director` =e.code AND (`state` =3 or `state` =4) ) as monthrefuse ,\n" +
+                    "(SELECT  COUNT(*) from `accepted`  a WHERE  a.`accept_time` >= '"+month_end+"' and a.`accept_time` < '"+month_end+"'  and  a.`clerk` =e.code ) as monthaccept,\n" +
+                    "(SELECT COUNT(*) FROM accepted a WHERE a.`end_date` >= '"+month_start+"' and a.`end_date` < '"+month_end+"'  and  a.`clerk` =e.code  and `state` =2) as monthend,\n" +
+                    "(SELECT COUNT(*) FROM accepted a WHERE a.`create_time`  >= '"+month_start+"' and a.`create_time`< '"+month_end+"'  and  a.`clerk` =e.code AND (`state` =3 or `state` =4) ) as monthrefuse ,\n" +
                     " (SELECT COUNT(*) FROM accepted a WHERE a.state = 1 and a.`director` = e.`code` AND a.business_type=0 ) as nowaccept_xindai,\n" +
                     " (SELECT COUNT(*) FROM accepted a WHERE a.state = 1 and a.`director` = e.`code` AND a.business_type=1 ) as nowaccept_diya,\n" +
                     " (SELECT COUNT(*) FROM accepted a WHERE a.state = 1 and a.`director` = e.`code` AND a.business_type=2 ) as nowaccept_zhiya,\n" +
