a95f01019ea982d8131e88ca6b29e5d8ee3f0b28
maojinhui_work@163.com <mjh521mjh>
2018-01-13 14:39:53 +0800

    数据统计--渠道部

--- src/main/java/com/rccf/controller/gzh/TopController.java
+++ src/main/java/com/rccf/controller/gzh/TopController.java
@@ -49,7 +49,7 @@ public class TopController {
         }
 //        int deid = Integer.valueOf(director_id);
 
-        String sql_data = "SELECT  e.`id` , e.`name`,e.`department` , e.`code` ,\n" +
+        String sql_data = "SELECT  e.`id` , e.`name`,e.`department` , e.`code` , e.role,\n" +
                 "(SELECT et.target*10000  from `employee_target` et WHERE et.`eid` = e.`id` ) as target , \n" +
                 "(SELECT  sum(a.`service_fee_actual`)  FROM `accepted` a WHERE a.`end_date`  >= '2018-01' and a.`end_date`< '2018-02'   and  a.`director` =e.code AND a.`state` =2) as monthyeji\n" +
                 "from `employee` e WHERE e.id="+director_id;
@@ -76,14 +76,13 @@ public class TopController {
         JSONArray array = JSON.parseArray(JSON.toJSONString(list));
         modelAndView.addObject("dupty_array" , array);
         if(Strings.isNullOrEmpty(director_id)){
-            director_id = list.get(0).toString();
+            dupty_id = list.get(0).toString();
         }
-//        int deid = Integer.valueOf(director_id);
 
-        String sql_data = "SELECT  e.`id` , e.`name`,e.`department` , e.`code` ,\n" +
+        String sql_data = "SELECT  e.`id` , e.`name`,e.`department` , e.`code` ,  e.role,\n" +
                 "(SELECT et.target*10000  from `employee_target` et WHERE et.`eid` = e.`id` ) as target , \n" +
-                "(SELECT  sum(a.`service_fee_actual`)  FROM `accepted` a WHERE a.`end_date`  >= '2018-01' and a.`end_date`< '2018-02'   and  a.`director` =e.code AND a.`state` =2) as monthyeji\n" +
-                "from `employee` e WHERE e.id="+director_id;
+                "(SELECT  sum(a.`service_fee_actual`)  FROM `accepted` a WHERE a.`end_date`  >= '2018-01' and a.`end_date`< '2018-02'   and  a.`deputy_director` =e.code AND a.`state` =2) as monthyeji\n" +
+                "from `employee` e WHERE e.id="+dupty_id;
         List list1 = baseService.queryBySqlFormatClass(Yeji.class, sql_data);
         if(list1!=null && list1.size()>0){
             modelAndView.addObject("data",list1.get(0));
